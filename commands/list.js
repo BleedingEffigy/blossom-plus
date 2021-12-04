aa381003d3eaf8a83662cc2e21f50fbc545c6298
Alex <ahern077@gmail.com>
2021-12-03 23:05:02 -0800

    list now works if default themes are installed

--- commands/list.js
+++ commands/list.js
@@ -8,7 +8,7 @@ const list = () => {
     var fileBuffer = fs.readFileSync(tailwindConfigLocation)
     // read the file into string
     tailwindConfig = fileBuffer.toString()
-    // finds opening bracket for the themes field
+    // finds opening bracket for the themes field and saves index infront
     themesOpenIndex = tailwindConfig.search(/themes: \[/) + 9
     // split the file and store the tail
     tailwindConfigTail = tailwindConfig.slice(themesOpenIndex, -1)
@@ -16,11 +16,13 @@ const list = () => {
     //this value is the index in the tail, so we add themesOpenIndex to compensate 
     themesCloseIndex = tailwindConfigTail.search(/]/) + themesOpenIndex
     // find the index of the custom themes opening bracket, inside the themes array
-    customThemesIndex = tailwindConfig.slice(themesOpenIndex).search('{') + themesOpenIndex
-    // store the string of the map
+    customThemesMapOpenIndex = tailwindConfig.slice(themesOpenIndex).search('{') + themesOpenIndex
+    // store the string of the map, replacing single quotes with double for the parser
     customThemesString = tailwindConfig.slice(themesOpenIndex, themesCloseIndex).replaceAll("'", "\"")
+    // find the closing bracket of the themes map
+    customThemesMapCloseIndex = customThemesString.lastIndexOf('}') + 1
     // turn that JSON string into an object 
-    customThemesObject = JSON.parse(customThemesString)
+    customThemesObject = JSON.parse(customThemesString.slice(0, customThemesMapCloseIndex))
     // list the names of the installed themes
     Object.keys(customThemesObject).forEach( keys => console.log(chalk.red.underline(keys)))
 
